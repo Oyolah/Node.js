06ecc44f89165c6bd836ebeed27e97790f1e844a
oyolah <oyolahmustapha2015@gmail.com>
2019-10-30 18:52:26 +0200

    add fuctionality

--- Week1/main.js
+++ Week1/main.js
@@ -1,24 +1,28 @@
 const http = require('http')
 
+let state = 10;
+
 const host = 'localhost';
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res)=>{
     if(req.url == '/state'){
         res.writeHead(300, {'Content-Type':'text/html'})
-        res.end('<html><head><body><h1>The State number is: 10</h1></body></head></html>')
+        res.end(`<html><head><body><h1>The State number is: ${state}</h1></body></head></html>`)
         }
     else if(req.url == '/add'){
             res.writeHead(300, {'Content-Type':'text/html'})
-            res.end('<html><head><body><h1>The add number is: 11</h1></body></head></html>')
+            state++
+            res.end(`<html><head><body><h1>The add number is: ${state}</h1></body></head></html>`)
             }
     else if(req.url == '/subtract'){
                 res.writeHead(300, {'Content-Type':'text/html'})
-                res.end('<html><head><body><h1>The subtract number is: 9</h1></body></head></html>')
+                state --
+                res.end(`<html><head><body><h1>The subtract number is: ${state}</h1></body></head></html>`)
             }
     else if(req.url == '/reset'){
                 res.writeHead(300, {'Content-Type':'text/html'})
-                res.end('<html><head><body><h1>The reset number is: 10</h1></body></head></html>')
+                res.end(`<html><head><body><h1>The reset number is: ${state}</h1></body></head></html>`)
             }
         else{
             res.writeHead(300, {'Content-Type':'text/html'})
